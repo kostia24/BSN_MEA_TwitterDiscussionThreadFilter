790640a545719889a47a2f79ba14e3021991712a
Kostia Hovyenko <konstantin.hovyenko@volia-software.com>
2017-01-23 13:46:22 +0200

    add comments

--- src/bsnThreadFilter.js
+++ src/bsnThreadFilter.js
@@ -1,25 +1,25 @@
 var showTwitter = true;
 var showUsers = true;
 var showLinks = true;
-var showEmbededYoutube = true;
+var showEmbeddedYoutube = true;
 var includeImages = false;
 
 var savedMsgIndexes = [];
 var opt = {};
 
-var msgCopy = $(".message").clone();
-msgCopy.children(".quote").remove();
+var msgCopy = $(".message").clone(); // copy all messages
+msgCopy.children(".quote").remove(); // remove all qoutes from copied posts
 
 msgCopy.each(function (i, e) {
-	var twitterPatern = /( @+)|( ‏@+)|twitterwidget/m;
+	var twitterPatern = /( @+)|( ‏@+)|twitterwidget/m; // pattern for post with @ character and and embedded twitter
 	var userUrlPatern = /href="\/user\//m;
-	var youtubePatern = /<iframe/m;
+	var youtubePatern = /<iframe/m; // iframe use for embedded youtube
 	var hrefPattern = "";
 
 	if (includeImages)
-		hrefPattern = /href="http(.*?)"/m;
+		hrefPattern = /href="http(.*?)"/m; // external link 
 	else
-		hrefPattern = /href="http(.(?!(\.png|\.jpg|\.jpeg|\.gif)))*"/m;
+		hrefPattern = /href="http(.(?!(\.png|\.jpg|\.jpeg|\.gif)))*"/m; // external link without images
 
 	var hasTwitter = twitterPatern.test(e.innerHTML);
 	var hasUserUrl = userUrlPatern.test(e.innerHTML);
@@ -37,18 +37,18 @@ msgCopy.each(function (i, e) {
 	if (showLinks && hasHref)
 		shouldRemove = false;
 
-	if (showEmbededYoutube && hasYoutube)
+	if (showEmbeddedYoutube && hasYoutube)
 		shouldRemove = false;
 
 	if (shouldRemove)
-		$(e).remove();
+		$(e).remove(); 
 	else
-		savedMsgIndexes.push(i);
+		savedMsgIndexes.push(i); // remember post id
 
 });
 
 var msg = $(".message");
 msg.each(function (i, e) {
 	if (savedMsgIndexes.indexOf(i) == -1)
-		$(e).parents("tr").remove();
+		$(e).parents("tr").remove(); // remove original post block
 })
